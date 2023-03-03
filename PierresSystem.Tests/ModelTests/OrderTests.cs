4b5e6a153237acb389fe886e7a8d17e30d438e4e
Molly Donegan <mollyrdonegan@gmail.com>
2023-03-03 12:04:42 -0800

    pass fourth Order test

--- PierresSystem.Tests/ModelTests/OrderTests.cs
+++ PierresSystem.Tests/ModelTests/OrderTests.cs
@@ -28,5 +28,28 @@ namespace PierresSystem.Tests
       string result = newOrder.OrderName;
       Assert.AreEqual(orderName, result);
     }
+
+    [TestMethod]
+    public void SetOrderName_SetsOrderName_String()
+    {
+      string orderName = "Princ1";
+      Order newOrder = new Order(orderName, "Prince Coffee");
+
+      string updatedOrderName = "Prince1";
+      newOrder.OrderName = updatedOrderName;
+      string result = newOrder.OrderName;
+
+      Assert.AreEqual(updatedOrderName, result);
+    }
+
+    [TestMethod]
+    public void GetVendorName_ReturnsVendorName_String()
+    {
+      string vendorName = "Prince Coffee";
+      Order newOrder = new Order("Prince1", vendorName);
+      string result = newOrder.VendorName;
+
+      Assert.AreEqual(vendorName, result);
+    }
   }
 }
