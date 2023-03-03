7dd38af8e339143f4b8861d6267630ae148c2155
Molly Donegan <mollyrdonegan@gmail.com>
2023-03-03 14:57:32 -0800

    refactor Order tests

--- PierresSystem.Tests/ModelTests/OrderTests.cs
+++ PierresSystem.Tests/ModelTests/OrderTests.cs
@@ -30,6 +30,17 @@ namespace PierresSystem.Tests
     }
 
     [TestMethod]
+    public void SetDescription_SetDescription_String()
+    {
+      string description = "croissant";
+      Order newOrder = new Order(description, "Prince Coffee", "Prince1", "five dollars", "march 2023");
+      string updatedDescription = "croissants";
+      newOrder.Description = updatedDescription;
+      string result = newOrder.Description;
+      Assert.AreEqual(updatedDescription, result);
+    }
+
+    [TestMethod]
     public void GetAll_ReturnsEmptyList_ItemList()
     {
       List<Order> newOrder = new List<Order> { };
@@ -49,6 +60,19 @@ namespace PierresSystem.Tests
       List<Order> newList = new List<Order> { newOrder1, newOrder2 };
       List<Order> result = Order.GetAll();
       CollectionAssert.AreEqual(newList, result);
+    }
+
+    [TestMethod]
+    public void GetId_OrderInstantiateWithAnIdAndGetterReturns_Int()
+    {
+      //Arrange
+      string description = "croissants";
+      Order newOrder = new Order(description, "Prince Coffee", "Prince1", "five dollars", "march 2023");
+      //Act
+      int result = newOrder.Id;
+
+      //Assert
+      Assert.AreEqual(1, result);
     }
   }
 }
