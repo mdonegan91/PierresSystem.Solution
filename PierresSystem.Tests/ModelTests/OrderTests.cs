4da1c7c927b0e64ad6c8e01ca4991c2dd530c8ac
Molly Donegan <mollyrdonegan@gmail.com>
2023-03-03 11:14:27 -0800

    set up inital files

---
+++ PierresSystem.Tests/ModelTests/OrderTests.cs
@@ -0,0 +1,23 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PierresSystem.Models;
+using System.Collections.Generic;
+using System;
+
+namespace PierresSystem.Tests
+{
+  [TestClass]
+  public class VendorTests : IDisposable
+  {
+    public void Dispose()
+    {
+      Vendor.ClearAll();
+    }
+
+    [TestMethod]
+    public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
+    {
+      Vendor newVendor = new Vendor("Prince Coffee");
+      Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+  }
+}
---
+++ PierresSystem/Models/Vendor.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+
+namespace PierresSystem.Models
+{
+  public class Vendor
+  {
+    private static List<Vendor> _instances = new List<Vendor> {};
+  }
+}
