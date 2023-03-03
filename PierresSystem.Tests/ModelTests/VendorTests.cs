aa26cef68b7c7eb605305993ba201d63c917ee24
Molly Donegan <mollyrdonegan@gmail.com>
2023-03-03 11:44:11 -0800

    pass fourth Vendor test

--- PierresSystem.Tests/ModelTests/VendorTests.cs
+++ PierresSystem.Tests/ModelTests/VendorTests.cs
@@ -37,5 +37,17 @@ namespace PierresSystem.Tests
       int result = newVendor.Id;
       Assert.AreEqual(1, result);
     }
+
+    [TestMethod]
+    public void GetAll_ReturnsAllVendorObjects_VendorList()
+    {
+      string name01 = "Prince Coffee";
+      string name02 = "Cathedral Coffee";
+      Vendor newVendor1 = new Vendor(name01);
+      Vendor newVendor2 = new Vendor(name02);
+      List<Vendor> newList = new List<Vendor> { newVendor1, newVendor2 };
+      List<Vendor> result = Vendor.GetAll();
+      CollectionAssert.AreEqual(newList, result);
+    }
   }
 }
